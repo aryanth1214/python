4f9170387d339fc9df0c786f1a68e41709a41ed0
devi447 <devisingh447@gmail.com.com>
2025-12-30 12:55:18 +0530

    first commit

--- hello.py
+++ hello.py
@@ -1,5 +1,64 @@
-print("Hello, World! This Aryan")
-name=Aryan Thakur
+name='aryan'
 age=12
 print(name)
-print(age)
+Price=100.2
+
+print(type(name))
+print(type(age))
+print(type(Price))
+
+#comments:
+#print("Hello, World!") 
+
+
+#arthematic operations:
+a=10
+b=4
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+h=a**b
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+
+#rational operations:
+a=10
+b=4
+print(a==b)#false
+print(a!=b)#true
+print(a>b)#true  
+print(a<b)#false
+#assignment operations:
+num=10
+num-=10
+print(num)
+
+num=10
+num*=10
+print(num)
+
+
+#logical operations:
+print(not(True))#false
+a=20
+b=10
+print(not(a>b))#false
+
+
+val1=True
+val2=False
+print(val1 and val2)#false
+
+print(a==b or a>b)#true
+
+#type conversion:
+num1=10
+num2=20.5
+print(num1+num2)
